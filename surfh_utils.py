dd3c2d616134c6bf6ee746a4c3924ce662ecd79b
Louise Breuval <louise.breuval@hotmail.fr>
2022-06-02 17:52:29 +0200

    Update surfh_utils.py
    I deleted the function :)

--- surfh_utils.py
+++ surfh_utils.py
@@ -19,39 +19,4 @@ from surfh_code import smallmrs as mrs
 from surfh_code import utils
 from surfh_code.algorithms import vox_reconstruction
 
-def fov():
-   fig, ax1 = plt.subplots(1, 1)
-   fov = mrs.ch1a.fov
-   ax1.plot(
-      [v.alpha for v in fov.vertices] + [fov.vertices[0].alpha],
-       [v.beta for v in fov.vertices] + [fov.vertices[0].beta],
-       "-",
-       alpha=1,
-       color="blue",
-   )
-   fov = mrs.ch2a.fov
-   ax1.plot(
-      [v.alpha for v in fov.vertices] + [fov.vertices[0].alpha],
-       [v.beta for v in fov.vertices] + [fov.vertices[0].beta],
-       "-",
-       alpha=1,
-       color="green",
-   )
-   fov = mrs.ch3a.fov
-   ax1.plot(
-      [v.alpha for v in fov.vertices] + [fov.vertices[0].alpha],
-       [v.beta for v in fov.vertices] + [fov.vertices[0].beta],
-       "-",
-       alpha=1,
-       color="yellow",
-   )
-   fov = mrs.ch4a.fov
-   ax1.plot(
-      [v.alpha for v in fov.vertices] + [fov.vertices[0].alpha],
-       [v.beta for v in fov.vertices] + [fov.vertices[0].beta],
-       "-",
-       alpha=1,
-       color="red",
-   )
-   ax1.invert_xaxis()
-   plt.show()
+
